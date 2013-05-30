439024b1e89819d7eaf49f5c20871f506c9db145
Scott Kruger <scott@chojin.org>
2013-05-29 22:09:39 -0500

    Added lastfm album/artist corrections

--- model.py
+++ model.py
@@ -1,30 +1,69 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import create_engine, func, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, backref
+from sqlalchemy.ext.hybrid import hybrid_property, Comparator
 import lastfm
 
 Base = declarative_base()
 engine = create_engine('sqlite:///:memory:', echo=False)
 Session = sessionmaker()
 
+class CaseInsensitiveComparator(Comparator):
+  def __eq__(self, other):
+    return func.lower(self.__clause_element__()) == func.lower(other)
+
 class Artist(Base):
   __tablename__ = 'artists'
 
   id = Column(Integer, primary_key=True)
-  name = Column(String)
+  name = Column(String, nullable=False)
 
   def __init__(self, name):
     self.name = name
+
+  @hybrid_property
+  def name_insensitive(self):
+    return self.name.lower()
+
+  @name_insensitive.comparator
+  def name_insensitive(cls):
+    return CaseInsensitiveComparator(cls.name)
 
 class Album(Base):
   __tablename__ = 'albums'
 
   id = Column(Integer, primary_key=True)
-  name = Column(String)
+  name = Column(String, nullable=False)
   playcount = Column(Integer)
   artist_id = Column(Integer, ForeignKey('artists.id'))
 
   artist = relationship('Artist', backref=backref('albums', order_by=id))
+
+  @hybrid_property
+  def name_insensitive(self):
+    return self.name.lower()
+
+  @name_insensitive.comparator
+  def name_insensitive(cls):
+    return CaseInsensitiveComparator(cls.name)
+
+class ArtistCorrection(Base):
+  __tablename__ = 'artist_corrections'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  artist_id = Column(Integer, ForeignKey('artists.id'))
+
+  artist = relationship('Artist', backref=backref('corrections', order_by=id))
+
+class AlbumCorrection(Base):
+  __tablename__ = 'album_corrections'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  album_id = Column(Integer, ForeignKey('albums.id'))
+
+  album = relationship('Album', backref=backref('corrections', order_by=id))
 
 class Scrobble(Base):
   __tablename__ = 'scrobbles'
--- mstat.py
+++ mstat.py
@@ -1,11 +1,15 @@
 from lastfm import LastFM, APIKEY
-from model import Artist, Album, Scrobble, Session, Base
+from model import Artist, AlbumCorrection, ArtistCorrection, Album, Scrobble, Session, Base
 
 import mpd
 from sqlalchemy import create_engine
 
 from collections import defaultdict
 from itertools import chain
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 def initialize_sqlalchemy():
   engine = create_engine('sqlite:///:memory:', echo=False)
@@ -29,26 +33,92 @@ def initialize_mpd(host=None, port=None):
 def initialize_lastfm():
   return LastFM(APIKEY)
 
-def insert_mpd_albums(session, client):
-  # TODO: Correct mpd names?
-  for artist_name in client.list('artist'):
-    artist = session.query(Artist).filter_by(name=artist_name).first()
-    if artist is None:
-      artist = Artist(artist_name)
-      session.add(artist)
+def correct_artist(name, lastfm):
+  logger.info('Attempting to find a correction for {}'.format(name))
 
-    for album_name in client.list('album', 'artist', artist_name):
+  resp = lastfm.query('artist.getCorrection', artist=name)
+  if 'corrections' in resp and isinstance(resp['corrections'], dict):
+    correct = resp['corrections']['correction']
+    if 'artist' in correct:
+      artist = Artist(name=correct['artist']['name'])
+      corrected = ArtistCorrection(name=name)
+
+      return (artist, corrected)
+
+  artist = Artist(name=name)
+  return (artist, None)
+
+def most_likely_album(name, artist, lastfm):
+  resp = lastfm.query('album.search', album=name)
+  if 'results' in resp and 'albummatches' in resp['results']:
+    albums = resp['results']['albummatches']['album']
+
+    albums_by_artist = defaultdict(list)
+    for album in albums:
+      if isinstance(album, dict):
+        albums_by_artist[album['artist']].append(album['name'])
+
+    if artist.name in albums_by_artist:
+      return min(albums_by_artist[artist.name], key = lambda alb: len(alb))
+
+  return None
+
+def correct_album(name, artist, lastfm, session):
+  logger.info('Attempt to find a correction for {}'.format(name))
+
+  likely_album = most_likely_album(name, artist, lastfm)
+  if likely_album:
+    album = session.query(Album).filter_by(name=likely_album).first()
+    if album:
+      corrected = None
+    else:
+      album = Album(name=likely_album, playcount=0)
+      corrected = AlbumCorrection(name=likely_album)
+  else:
+    album = Album(name=name, playcount=0)
+    corrected = None
+
+  return (album, corrected)
+
+def insert_mpd_albums(session, mpdclient, lastfm):
+  for artist_name in mpdclient.list('artist'):
+    artist = session.query(Artist).\
+                     filter_by(name_insensitive = artist_name).\
+                     first()
+    if artist is None:
+      artist = session.query(ArtistCorrection).\
+               filter_by(name_insensitive = artist_name).first()
+      if artist is None:
+        artist, correction = correct_artist(artist_name, lastfm)
+        session.add(artist)
+        if correction:
+          artist.corrections.append(correction)
+          session.add(correction)
+
+    for album_name in mpdclient.list('album', 'artist', artist_name):
+      # Look in database
       album = session.query(Album).join(Artist).\
                       filter(Album.name==album_name).first()
       if album is None:
-        album = Album(name=album_name, playcount=0)
-        artist.albums.append(album)
-        session.add(album)
+        # try to find an existing correction for this name
+        correction = session.query(AlbumCorrection).\
+                     filter_by(name=album_name).first()
+        if correction is None:
+          # Try to find a correction from LastFM
+          album, correction = correct_album(album_name, artist, lastfm, session)
+          session.add(album)
+          if correction:
+            album.corrections.append(correction)
+            session.add(correction)
+
+          artist.albums.append(album)
 
   session.commit()
 
 def insert_lastfm_albums(session, lastfm, user):
   for resp in lastfm.query_all('library.getAlbums', 'albums', user=user):
+    if 'albums' not in resp:
+      continue
     for album in resp['albums']['album']:
       if 'artist' in album:
         artist_name = album['artist']['name']
@@ -72,7 +142,7 @@ def insert_lastfm_albums(session, lastfm, user):
 
 def initialize_database(session, mpdclient, lastfm, user):
   insert_lastfm_albums(session, lastfm, user)
-  insert_mpd_albums(session, mpdclient)
+  insert_mpd_albums(session, mpdclient, lastfm)
 
 def run():
   session = initialize_sqlalchemy()
