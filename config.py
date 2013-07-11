e4c2f80766ae214426e52b160b6c22d8b9b5e835
Scott Kruger <skruger@yahoo-inc.com>
2013-07-11 14:33:41 -0700

    pep8; added highcolor option

--- config.py
+++ config.py
@@ -3,69 +3,76 @@ from os.path import expanduser, expandvars
 from time import time
 
 
-SECONDS_IN_DAY = 24*3600
+SECONDS_IN_DAY = 24 * 3600
 CONFIG_PATHS = [
-  '$HOME/.suggestive.conf',
-  '/etc/suggestive.conf',
- ]
+    '$HOME/.suggestive.conf',
+    '/etc/suggestive.conf',
+]
 
 
 def expand(path):
-  """Expand a unix path"""
-  return expanduser(expandvars(path))
+    """Expand a unix path"""
+    return expanduser(expandvars(path))
 
 
 class Config(object):
-  """Suggestive configuration object"""
 
-  DEFAULTS = dict(
-    general = dict(
-      database = '$HOME/.suggestive/music.db',
-    ),
-    mpd = dict(
-      host = 'localhost',
-      port = 6600,
-    ),
-    lastfm = dict(
-      scrobble_days = 180,
-      # user
-      # api_key
-    ),
-  )
+    """Suggestive configuration object"""
 
-  def __init__(self, path = None):
-    parser = ConfigParser()
-    parser.read_dict(self.DEFAULTS)
-
-    paths = CONFIG_PATHS
-    if path:
-      paths = [path] + CONFIG_PATHS
-
-    parser.read(map(expand, paths))
-
-    self.parser = parser
-
-  def mpd(self):
-    """Return (host, port)"""
-    mpd = self.parser['mpd']
-    return (
-      mpd['host'],
-      mpd.getint('port'),
+    DEFAULTS = dict(
+        general=dict(
+            conf_dir='$HOME/.suggestive',
+            database='$HOME/.suggestive/music.db',
+            highcolor=True,
+        ),
+        mpd=dict(
+            host='localhost',
+            port=6600,
+        ),
+        lastfm=dict(
+            scrobble_days=180,
+            # user
+            # api_key
+        ),
     )
 
-  def lastfm_user(self):
-    """Return LastFM user name"""
-    return self.parser['lastfm']['user']
+    def __init__(self, path=None):
+        parser = ConfigParser()
+        parser.read_dict(self.DEFAULTS)
 
-  def lastfm_apikey(self):
-    """Return LastFM API key"""
-    return self.parser['lastfm']['api_key']
+        paths = CONFIG_PATHS
+        if path:
+            paths = [path] + CONFIG_PATHS
 
-  def scrobble_retention(self):
-    """Return seconds to keep LastFM scrobbles"""
-    ret = self.parser['lastfm'].getint('scrobble_days') * SECONDS_IN_DAY
-    return int(time() - ret)
+        parser.read(map(expand, paths))
 
-  def database(self):
-    """Return the path to the database file"""
-    return expand(self.parser['general']['database'])
+        self.parser = parser
+
+    def mpd(self):
+        """Return (host, port)"""
+        mpd = self.parser['mpd']
+        return (
+            mpd['host'],
+            mpd.getint('port'),
+        )
+
+    def lastfm_user(self):
+        """Return LastFM user name"""
+        return self.parser['lastfm']['user']
+
+    def lastfm_apikey(self):
+        """Return LastFM API key"""
+        return self.parser['lastfm']['api_key']
+
+    def scrobble_retention(self):
+        """Return seconds to keep LastFM scrobbles"""
+        ret = self.parser['lastfm'].getint('scrobble_days') * SECONDS_IN_DAY
+        return int(time() - ret)
+
+    def database(self):
+        """Return the path to the database file"""
+        return expand(self.parser['general']['database'])
+
+    def use_256_colors(self):
+        """Return True if the terminal should be set to 256 colors"""
+        return self.parser.getboolean('general', 'highcolor')
