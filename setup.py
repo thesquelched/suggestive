d23f20ad16bf44bebe78aec711101d339a79650b
Scott Kruger <skruger@yahoo-inc.com>
2013-09-23 07:57:23 -0700

    Added minor package version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name='suggestive',
-    version='0.1',
+    version='0.1.0',
     description='Python MPD client with integrated Last.FM support',
     author='Scott Kruger',
     author_email='thesquelched+python@gmail.com',
