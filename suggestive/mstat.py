thesquelched/suggestive

commit a50b32c6f01b18af8c04d2c1f1826ad87f91dc38

Fixed track loading issue

suggestive/mstat.py
@@ -90,9 +90,10 @@ class ScrobbleLoader(object):
 
         db_track = session.query(Track).\
             join(Artist, Album).\
-            filter(Artist.name_insensitive == artist).\
-            filter(Album.name_insensitive == album).\
-            filter(Track.name_insensitive == track).\
+            filter(
+                Artist.name_insensitive == artist and
+                Album.name_insensitive == album and
+                Track.name_insensitive == track).\
             first()
 
         if db_track:
@@ -155,11 +156,13 @@ class MpdLoader(object):
             if not album:
                 continue
 
-            logger.debug("Loading {} tracks from album '{}'".format(
-                len(info_list), album))
+            logger.debug("Loading {} tracks from '{} - {}'".format(
+                len(info_list), db_artist.name, album))
 
-            db_album = session.query(Album).filter_by(
-                name_insensitive=album).first()
+            db_album = session.query(Album).\
+                filter(Album.name_insensitive == album).\
+                filter(Album.artist == db_artist).\
+                first()
             if not db_album:
                 db_album = Album(name=album)
                 db_artist.albums.append(db_album)
@@ -226,6 +229,25 @@ class MpdLoader(object):
                     "directories: {}".format(
                         album.artist.name, album.name, ', '.join(dirs)))
 
+    def segregate_track_info(self, missing_info):
+        by_artist = defaultdict(list)
+        for info in missing_info:
+            by_artist[info.get('artist')].append(info)
+
+        by_artist_album = defaultdict(lambda: defaultdict(list))
+        for artist, info_list in by_artist.items():
+            for info in info_list:
+                by_artist_album[artist][info.get('album')].append(info)
+
+        for artist, by_album in by_artist_album.items():
+            for album, tracks in by_album.items():
+                track_txt = 'Tracks:\n  {}'.format(
+                    '\n  '.join(info['file'] for info in tracks))
+                logger.debug('Tracks for {} - {}:\n  '.format(
+                    artist, album, track_txt))
+
+        return by_artist_album
+
     def load(self, session):
         files_in_mpd = set(self.mpd.list('file'))
         files_in_db = set(item.filename for item in session.query(
@@ -243,15 +265,7 @@ class MpdLoader(object):
                 chain.from_iterable(self.mpd.listallinfo(path)
                                     for path in missing))
 
-            by_artist = defaultdict(list)
-            for info in missing_info:
-                by_artist[info.get('artist')].append(info)
-
-            by_artist_album = defaultdict(lambda: defaultdict(list))
-            for artist, info_list in by_artist.items():
-                for info in info_list:
-                    by_artist_album[artist][info.get('album')].append(info)
-
+            by_artist_album = self.segregate_track_info(missing_info)
             self.load_by_artist_album(session, by_artist_album)
 
         self.remove_duplicates(session)
@@ -286,8 +300,9 @@ class TrackInfoLoader(object):
     def find_track(self, session, artist, track):
         return session.query(Track).\
             join(Artist).\
-            filter(Track.name_insensitive == track).\
-            filter(Artist.name_insensitive == artist).\
+            filter(
+                Track.name_insensitive == track and
+                Artist.name_insensitive == artist).\
             first()
 
     def find_artist(self, session, artist):

commit 4eb909ad6a6e3f56e6cab88dcd3d884a9522ce74

Fixed a database regression introduced in a50b32c6f01b18af8c04d2c1f1826ad87f91dc38

suggestive/mstat.py
@@ -56,8 +56,8 @@ class ScrobbleLoader(object):
 
         db_scrobble_info = session.query(ScrobbleInfo).\
             filter(
-                ScrobbleInfo.title_insensitive == track and
-                ScrobbleInfo.artist_insensitive == artist and
+                ScrobbleInfo.title_insensitive == track,
+                ScrobbleInfo.artist_insensitive == artist,
                 ScrobbleInfo.album_insensitive == album).\
             first()
 
@@ -89,10 +89,11 @@ class ScrobbleLoader(object):
         session.add(scrobble)
 
         db_track = session.query(Track).\
-            join(Artist, Album).\
+            join(Track.artist).\
+            join(Track.album).\
             filter(
-                Artist.name_insensitive == artist and
-                Album.name_insensitive == album and
+                Artist.name_insensitive == artist,
+                Album.name_insensitive == album,
                 Track.name_insensitive == track).\
             first()
 
@@ -232,7 +233,9 @@ class MpdLoader(object):
     def segregate_track_info(self, missing_info):
         by_artist = defaultdict(list)
         for info in missing_info:
-            by_artist[info.get('artist')].append(info)
+            artist = info.get('albumartist', info.get('artist'))
+
+            by_artist[artist].append(info)
 
         by_artist_album = defaultdict(lambda: defaultdict(list))
         for artist, info_list in by_artist.items():
@@ -270,10 +273,6 @@ class MpdLoader(object):
 
         self.remove_duplicates(session)
 
-    def initialize(self, session):
-        for artist in self.mpd.list('artist'):
-            self.load_artist(session, artist)
-
 
 class TrackInfoLoader(object):
 
@@ -301,7 +300,7 @@ class TrackInfoLoader(object):
         return session.query(Track).\
             join(Artist).\
             filter(
-                Track.name_insensitive == track and
+                Track.name_insensitive == track,
                 Artist.name_insensitive == artist).\
             first()
 