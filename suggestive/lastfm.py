9928133ed6e872fe5ae5edda4084a48112d79959
Scott Kruger <skruger@yahoo-inc.com>
2014-04-10 11:01:33 -0500

    Fixed off-by-one error in query_all

--- suggestive/lastfm.py
+++ suggestive/lastfm.py
@@ -128,8 +128,9 @@ Press Enter to continue...""".format(self.key, self.token)
         # Start to yield responses
         yield resp
 
-        for pageno in range(1, n_pages):
-            kwArgs.update({'page': pageno})
+        # Pages count from 1
+        for pageno in range(n_pages):
+            kwArgs.update({'page': pageno + 1})
             yield self.query(method, **kwArgs)
 
     def _sign(self, **params):
